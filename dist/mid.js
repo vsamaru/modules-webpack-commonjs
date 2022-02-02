6f0c628a81c569355dec5be62049f6423cac9123
vsamaru <vsamaru@gmail.com>
2022-02-02 05:19:13 +0200

    Update mid.js

--- dist/mid.js
+++ dist/mid.js
@@ -30,7 +30,7 @@ globalThis.require_upd = __commonJS({
       if (r.cf)
         r = await r.clone().json();
       globalThis.req = r;   
-
+console.info(req)
       req[Object.keys(req)[1]].type = Object.keys(req)[1];
       req = req[Object.keys(req)[1]];
       req.from = req.chat || req.from;
